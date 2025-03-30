9642ca568da9b33cf59ab4148a9858e1b1b66982
Harshita26705 <144931247+Harshita26705@users.noreply.github.com>
2025-03-30 20:55:29 +0530

    Create server.py
    Signed-off-by: Harshita26705 <144931247+Harshita26705@users.noreply.github.com>

---
+++ server.py
@@ -0,0 +1,11 @@
+from flask import Flask,
+jsonify
+
+app= Flask(__name__)
+
+@app.route('/')
+def home():
+  return jasonify({"message":"RegenWaste Flask App is Running!"})
+
+if __name__= '__main__':
+  app.run(host='0.0.0.0',port=10000)
