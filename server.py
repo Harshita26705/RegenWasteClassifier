ca225320be030f05c82cab008ed7063cf7c1c7f1
Harshita26705 <144931247+Harshita26705@users.noreply.github.com>
2025-03-31 11:20:27 +0530

    Update server.py
    Signed-off-by: Harshita26705 <144931247+Harshita26705@users.noreply.github.com>

--- server.py
+++ server.py
@@ -1,10 +1,11 @@
-from flask import Flask, jsonify
+from flask import Flask
+from flask import jsonify
 
 app= Flask(__name__)
 
 @app.route('/')
 def home():
-  return jasonify({"message":"RegenWaste Flask App is Running!"})
+  return jasonify({"message": "RegenWaste Flask App is Running!"})
 
 if __name__=='__main__':
   app.run(host='0.0.0.0',port=10000)
